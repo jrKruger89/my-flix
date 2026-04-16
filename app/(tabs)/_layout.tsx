be6aafc79931b5b938ef2dbbe3166fde13ea95c5
NathaliaWitDolatko <nathaliawitdolatko@gmail.com>
2026-04-16 13:55:41 +0200

    Implement fonts

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -20,7 +20,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          title: "My Flix",
           tabBarIcon: ({ color, focused }) => (
             <Ionicons
               name={focused ? "home-sharp" : "home-outline"}
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,105 +1,124 @@
-import React from "react";
+import * as Font from "expo-font";
+import React, { useEffect, useState } from "react";
 import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync({
+        K2D: require("../../assets/fonts/K2D-Regular.ttf"),
+      });
+      setFontsLoaded(true);
+    };
+
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
   return (
-    <View style={styles.container}>
-      <Text style={styles.rowTitle}>Row 1</Text>
-      <ScrollView horizontal={true} contentContainerStyle={styles.scrollRow}>
-        <View>
-          <Image
-            source={{ uri: "https://thispersondoesnotexist.com/" }}
-            style={styles.image}
-          />
-          <Text style={styles.rowTitle}>Movie Title</Text>
-        </View>
-        <View>
-          <Image
-            source={{ uri: "https://thispersondoesnotexist.com/" }}
-            style={styles.image}
-          />
-          <Text style={styles.rowTitle}>Movie Title</Text>
-        </View>
-        <View>
-          <Image
-            source={{ uri: "https://thispersondoesnotexist.com/" }}
-            style={styles.image}
-          />
-          <Text style={styles.rowTitle}>Movie Title</Text>
-        </View>
-        <View>
-          <Image
-            source={{ uri: "https://thispersondoesnotexist.com/" }}
-            style={styles.image}
-          />
-          <Text style={styles.rowTitle}>Movie Title</Text>
-        </View>
-      </ScrollView>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.rowTitle}>Currently Watching</Text>
+        <ScrollView horizontal={true} contentContainerStyle={styles.scrollRow}>
+          <View>
+            <Image
+              source={{ uri: "https://thispersondoesnotexist.com/" }}
+              style={styles.image}
+            />
+            <Text style={styles.movieTitle}>Movie Title</Text>
+          </View>
+          <View>
+            <Image
+              source={{ uri: "https://thispersondoesnotexist.com/" }}
+              style={styles.image}
+            />
+            <Text style={styles.movieTitle}>Movie Title</Text>
+          </View>
+          <View>
+            <Image
+              source={{ uri: "https://thispersondoesnotexist.com/" }}
+              style={styles.image}
+            />
+            <Text style={styles.movieTitle}>Movie Title</Text>
+          </View>
+          <View>
+            <Image
+              source={{ uri: "https://thispersondoesnotexist.com/" }}
+              style={styles.image}
+            />
+            <Text style={styles.movieTitle}>Movie Title</Text>
+          </View>
+        </ScrollView>
 
-      <Text style={styles.rowTitle}>Row 2</Text>
-      <ScrollView horizontal={true} contentContainerStyle={styles.scrollRow}>
-        <View>
-          <Image
-            source={{ uri: "https://thispersondoesnotexist.com/" }}
-            style={styles.image}
-          />
-          <Text style={styles.rowTitle}>Movie Title</Text>
-        </View>
-        <View>
-          <Image
-            source={{ uri: "https://thispersondoesnotexist.com/" }}
-            style={styles.image}
-          />
-          <Text style={styles.rowTitle}>Movie Title</Text>
-        </View>
-        <View>
-          <Image
-            source={{ uri: "https://thispersondoesnotexist.com/" }}
-            style={styles.image}
-          />
-          <Text style={styles.rowTitle}>Movie Title</Text>
-        </View>
-        <View>
-          <Image
-            source={{ uri: "https://thispersondoesnotexist.com/" }}
-            style={styles.image}
-          />
-          <Text style={styles.rowTitle}>Movie Title</Text>
-        </View>
-      </ScrollView>
+        <Text style={styles.rowTitle}>Watchlist</Text>
+        <ScrollView horizontal={true} contentContainerStyle={styles.scrollRow}>
+          <View>
+            <Image
+              source={{ uri: "https://thispersondoesnotexist.com/" }}
+              style={styles.image}
+            />
+            <Text style={styles.movieTitle}>Movie Title</Text>
+          </View>
+          <View>
+            <Image
+              source={{ uri: "https://thispersondoesnotexist.com/" }}
+              style={styles.image}
+            />
+            <Text style={styles.movieTitle}>Movie Title</Text>
+          </View>
+          <View>
+            <Image
+              source={{ uri: "https://thispersondoesnotexist.com/" }}
+              style={styles.image}
+            />
+            <Text style={styles.movieTitle}>Movie Title</Text>
+          </View>
+          <View>
+            <Image
+              source={{ uri: "https://thispersondoesnotexist.com/" }}
+              style={styles.image}
+            />
+            <Text style={styles.movieTitle}>Movie Title</Text>
+          </View>
+        </ScrollView>
 
-      <Text style={styles.rowTitle}>Row 3</Text>
-      <ScrollView horizontal={true} contentContainerStyle={styles.scrollRow}>
-        <View>
-          <Image
-            source={{ uri: "https://thispersondoesnotexist.com/" }}
-            style={styles.image}
-          />
-          <Text style={styles.rowTitle}>Movie Title</Text>
-        </View>
-        <View>
-          <Image
-            source={{ uri: "https://thispersondoesnotexist.com/" }}
-            style={styles.image}
-          />
-          <Text style={styles.rowTitle}>Movie Title</Text>
-        </View>
-        <View>
-          <Image
-            source={{ uri: "https://thispersondoesnotexist.com/" }}
-            style={styles.image}
-          />
-          <Text style={styles.rowTitle}>Movie Title</Text>
-        </View>
-        <View>
-          <Image
-            source={{ uri: "https://thispersondoesnotexist.com/" }}
-            style={styles.image}
-          />
-          <Text style={styles.rowTitle}>Movie Title</Text>
-        </View>
-      </ScrollView>
-    </View>
+        <Text style={styles.rowTitle}>Reccomended</Text>
+        <ScrollView horizontal={true} contentContainerStyle={styles.scrollRow}>
+          <View>
+            <Image
+              source={{ uri: "https://thispersondoesnotexist.com/" }}
+              style={styles.image}
+            />
+            <Text style={styles.movieTitle}>Movie Title</Text>
+          </View>
+          <View>
+            <Image
+              source={{ uri: "https://thispersondoesnotexist.com/" }}
+              style={styles.image}
+            />
+            <Text style={styles.movieTitle}>Movie Title</Text>
+          </View>
+          <View>
+            <Image
+              source={{ uri: "https://thispersondoesnotexist.com/" }}
+              style={styles.image}
+            />
+            <Text style={styles.movieTitle}>Movie Title</Text>
+          </View>
+          <View>
+            <Image
+              source={{ uri: "https://thispersondoesnotexist.com/" }}
+              style={styles.image}
+            />
+            <Text style={styles.movieTitle}>Movie Title</Text>
+          </View>
+        </ScrollView>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -111,8 +130,15 @@ const styles = StyleSheet.create({
   },
   rowTitle: {
     color: "#fff",
-    fontSize: 18,
+    fontSize: 20,
     marginVertical: 10,
+    fontFamily: "K2D",
+  },
+  movieTitle: {
+    color: "#fff",
+    fontSize: 15,
+    marginVertical: 10,
+    fontFamily: "K2D",
   },
   scrollRow: {
     flexDirection: "row",
@@ -120,7 +146,7 @@ const styles = StyleSheet.create({
   },
   image: {
     width: 100,
-    height: 100,
+    height: 130,
     borderRadius: 10,
     marginHorizontal: 10,
   },
